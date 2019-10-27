2ba7bdf261f18cb0011f0b228994f30cded1e9d9
Mirco53 <mircoboerm@gmx.de>
2019-10-27 15:17:26 +0100

    created basic UI

--- src/main/java/hsb/msc/cloud/CloudApp.java
+++ src/main/java/hsb/msc/cloud/CloudApp.java
@@ -1,29 +1,74 @@
-package hsb.msc;
+package hsb.msc.cloud;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
-/**
- * JavaFX App
- */
 public class CloudApp extends Application {
+    private BorderPane root;
 
-    @Override
-    public void start(Stage stage) {
-        var javaVersion = SystemInfo.javaVersion();
-        var javafxVersion = SystemInfo.javafxVersion();
+    private void initUI(Stage stage) {
 
-        var label = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        var scene = new Scene(new StackPane(label), 640, 480);
+        root = new BorderPane();
+
+        root.setTop(getMenuBar());
+        root.setCenter(getTabPane());
+
+        Scene scene = new Scene(root, 800, 600);
+
+        stage.setTitle("Cloud Architecture Optimizer");
         stage.setScene(scene);
         stage.show();
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        initUI(primaryStage);
+    }
+
+    private MenuBar getMenuBar() {
+        MenuBar menuBar = new MenuBar();
+
+        Menu menuFile = new Menu("File");
+        MenuItem menuItemSaveAs = new MenuItem("Save As");
+        MenuItem menuItemExit = new MenuItem("Exit");
+        menuFile.getItems().add(menuItemSaveAs);
+        menuFile.getItems().add(menuItemExit);
+
+        Menu menuEdit = new Menu("Edit");
+        Menu menuView = new Menu("View");
+
+        Menu menuHelp = new Menu("Help");
+        MenuItem menuItemAbout = new MenuItem("About");
+        menuHelp.getItems().add(menuItemAbout);
+
+        menuBar.getMenus().add(menuFile);
+        menuBar.getMenus().add(menuEdit);
+        menuBar.getMenus().add(menuView);
+        menuBar.getMenus().add(menuHelp);
+
+        return menuBar;
+    }
+
+    private TabPane getTabPane() {
+        TabPane tabPane = new TabPane();
+        Tab tabConfig = new Tab("Configurations");
+        tabConfig.setClosable(false);
+        Tab tabArch = new Tab("Architecture");
+        tabArch.setClosable(false);
+
+        tabPane.getTabs().add(tabConfig);
+        tabPane.getTabs().add(tabArch);
+
+        return tabPane;
     }
 
     public static void main(String[] args) {
         launch();
     }
-
 }
