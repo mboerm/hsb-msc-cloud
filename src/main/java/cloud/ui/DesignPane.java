2c2b9c6b77f75232769c6b37df1524903be37965
Mirco53 <mircoboerm@gmx.de>
2019-11-01 19:09:57 +0100

    fixed variables to local; fixed init call

--- src/main/java/cloud/ui/DesignPane.java
+++ src/main/java/cloud/ui/DesignPane.java
@@ -6,44 +6,34 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 
-public class DesignPane extends BorderPane {
+class DesignPane extends BorderPane {
 
-    private HBox designButtons;
-    private Button designAdd;
-    private Button designRemove;
-    private Button designEdit;
-    private Button designReset;
-
-    private VBox designComponents;
-
-
-    private StackPane designArea;
-
-    public DesignPane() {
-        designButtons = new HBox();
-        designComponents = new VBox();
-        designArea = new StackPane();
-
-        this.setTop(designButtons);
-        this.setRight(designComponents);
-        this.setCenter(designArea);
+    DesignPane() {
+        this.setTop(initDesignButtons());
+        this.setRight(initDesignComponents());
+        this.setCenter(initDesignArea());
     }
 
-    private void initDesignButtons() {
-        designAdd = new Button("+");
-        designRemove = new Button("-");
-        designEdit = new Button("E");
-        designReset = new Button("R");
+    private HBox initDesignButtons() {
+        HBox designButtons = new HBox();
+        Button designAdd = new Button("+");
+        Button designRemove = new Button("-");
+        Button designEdit = new Button("E");
+        Button designReset = new Button("R");
 
         designButtons.setSpacing(10);
         designButtons.getChildren().addAll(designAdd, designRemove, designEdit, designReset);
+
+        return designButtons;
     }
 
-    private void initDesignComponents() {
-
+    private VBox initDesignComponents() {
+        VBox designComponents = new VBox();
+        return designComponents;
     }
 
-    private void initDesignArea() {
-
+    private StackPane initDesignArea() {
+        StackPane designArea = new StackPane();
+        return designArea;
     }
 }
