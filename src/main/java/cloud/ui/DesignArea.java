86c9faa7eee0e3281079aa0e6808c7bdc56f0db7
Mirco53 <mircoboerm@gmx.de>
2019-11-02 11:31:50 +0100

    design pane is now design area

---
+++ src/main/java/cloud/ui/DesignArea.java
@@ -0,0 +1,15 @@
+package cloud.ui;
+
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.layout.StackPane;
+import javafx.scene.text.Text;
+
+class DesignArea extends StackPane {
+
+    DesignArea() {
+        Text properties = new Text("Design Area");
+        getChildren().add(properties);
+        setAlignment(Pos.CENTER);
+    }
+}
