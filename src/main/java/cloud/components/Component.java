0f6e1836feb74bfa711726dd80abfe98f5a455d7
Mirco <mircoboerm@gmx.de>
2019-11-02 16:43:41 +0100

    added default component class

---
+++ src/main/java/cloud/components/Component.java
@@ -0,0 +1,27 @@
+package cloud.components;
+
+class Component {
+
+    private String name;
+    private componentCategory category;
+    protected enum componentCategory {DEFAULT, VM, STORAGE, DATABASE};
+
+    Component() {
+        name = "Default";
+        category = componentCategory.DEFAULT;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public componentCategory getCategory() {
+        return this.category;
+    }
+    public void setCategory(componentCategory category) {
+        this.category = category;
+    }
+}
