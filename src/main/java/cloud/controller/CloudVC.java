3afe91d22ec722abd009001d40c94865b5e8056a
Mirco <mircoboerm@gmx.de>
2019-11-03 19:47:05 +0100

    updated controller with session

--- src/main/java/cloud/controller/CloudVC.java
+++ src/main/java/cloud/controller/CloudVC.java
@@ -1,20 +1,45 @@
 package cloud.controller;
 
-import cloud.model.DataBean;
+import cloud.model.Session;
 import cloud.model.StageManager;
 import cloud.view.CloudView;
+import javafx.stage.FileChooser;
+
+import java.io.File;
 
 public class CloudVC {
 
     // Model
-    private DataBean dataBean;
+    private Session session = null;
 
     // View
     private CloudView view;
 
-    public CloudVC(DataBean dataBean) {
-        this.dataBean = dataBean;
+    public CloudVC(Session session) {
+        this.session = session;
         this.view = new CloudView();
+
+        view.getMenuFile().getItems().get(0).setOnAction(actionEvent -> {
+        });
+
+        view.getMenuFile().getItems().get(1).setOnAction(actionEvent -> {
+            FileChooser fileChooser = new FileChooser();
+            File selectedFile = fileChooser.showOpenDialog(StageManager.getInstance().getPrimaryStage());
+        });
+
+        view.getMenuFile().getItems().get(2).setOnAction(actionEvent -> {
+            FileChooser fileChooser = new FileChooser();
+            File savedFile = fileChooser.showSaveDialog(StageManager.getInstance().getPrimaryStage());
+        });
+
+        view.getMenuFile().getItems().get(3).setOnAction(actionEvent -> System.exit(0));
+    }
+
+    public void newSession() {
+        this.session = new Session();
+    }
+    public Session getSession() {
+        return this.session;
     }
 
     public void show() {
