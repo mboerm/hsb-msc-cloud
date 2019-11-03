57751593ce16d820023d3067b78917c921905494
Mirco <mircoboerm@gmx.de>
2019-11-03 17:22:04 +0100

    updated component pane with alignments

--- src/main/java/cloud/view/dialogs/PaneComponent.java
+++ src/main/java/cloud/view/dialogs/PaneComponent.java
@@ -1,6 +1,7 @@
-package cloud.ui.dialogs;
+package cloud.view.dialogs;
 
-import javafx.geometry.Pos;
+import javafx.geometry.HPos;
+import javafx.geometry.Insets;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
@@ -10,13 +11,17 @@ public class PaneComponent extends GridPane {
     private TextField nameField;
 
     PaneComponent() {
-        setAlignment(Pos.CENTER);
+        setHgap(10);
+        setVgap(10);
+        setPadding(new Insets(10, 10, 10, 10));
 
         Label nameLabel = new Label("Name: ");
         nameField = new TextField();
 
         add(nameLabel, 0, 0);
         add(nameField, 1, 0);
+        setHalignment(nameLabel, HPos.RIGHT);
+        setHalignment(nameField, HPos.LEFT);
     }
 
     public String getName() {
