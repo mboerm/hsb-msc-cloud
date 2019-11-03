0a3722105f0e9d023353c65526d511007e13336c
Mirco53 <mircoboerm@gmx.de>
2019-11-03 12:34:03 +0100

    updated constants

--- src/main/java/cloud/constants/Consts.java
+++ src/main/java/cloud/constants/Consts.java
@@ -3,19 +3,26 @@ package cloud.constants;
 public final class Consts {
 
     public static final String APP_TITLE = "Cloud Architecture Optimizer";
-
     public static final int WINDOW_DEFAULT_WIDTH = 1024;
     public static final int WINDOW_DEFAULT_HEIGHT = 768;
     public static final int WINDOW_MIN_WIDTH = 800;
     public static final int WINDOW_MIN_HEIGHT = 600;
+
+    public static final String[] COMPONENTS = new String[]{
+            "Virtual Machine",
+            "Storage",
+            "Database"
+    };
 
     public static final String ABOUT_TEXT =
             "This tool is the result of the master thesis.";
 
 
     private Consts(){
-        //this prevents even the native class from
-        //calling this actor as well :
+        /*
+        this prevents even the native class from
+        calling this actor as well :
+        */
         throw new AssertionError();
     }
 }
