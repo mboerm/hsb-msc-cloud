fcd7236efecabbb360a9561d03c4b89f8105ee0c
Mirco <mircoboerm@gmx.de>
2019-11-03 17:46:51 +0100

    created Stage Manager

---
+++ src/main/java/cloud/model/StageManager.java
@@ -0,0 +1,32 @@
+package cloud.model;
+
+import javafx.stage.Stage;
+
+public class StageManager {
+
+    private static StageManager instance;
+    private static Stage primaryStage;
+
+    /** Konstruktor ist privat, Klasse darf nicht von außen instanziiert werden. */
+    private StageManager() {
+    }
+
+    /**
+     * Statische Methode 'getInstance()Ä liefert die einzige Instanz der Klasse zurück.
+     * Ist synchronisiert und somit thread-sicher.
+     */
+    public synchronized static StageManager getInstance() {
+        if (instance == null) {
+            instance = new StageManager();
+        }
+        return instance;
+    }
+
+    public Stage getPrimaryStage() {
+        return primaryStage;
+    }
+
+    public void setPrimaryStage(Stage primaryStage) {
+        StageManager.primaryStage = primaryStage;
+    }
+}
