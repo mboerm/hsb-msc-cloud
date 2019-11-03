00c2bde415a90ad4e3fe107d298c4f5ccffa7175
Mirco <mircoboerm@gmx.de>
2019-11-03 17:47:25 +0100

    updated cloud app to use stage manager

--- src/main/java/cloud/main/CloudApp.java
+++ src/main/java/cloud/main/CloudApp.java
@@ -2,6 +2,7 @@ package cloud.main;
 
 import cloud.controller.CloudVC;
 import cloud.model.DataBean;
+import cloud.model.StageManager;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
@@ -17,9 +18,9 @@ public class CloudApp extends Application {
     public void start(Stage primaryStage) throws Exception {
         Locale.setDefault(new Locale("en"));
 
-        // session scope /application scope Beans initialisieren!
-        // muss von Controller zu Controller weitergegeben werden
-        DataBean dataBean = new DataBean(primaryStage);
+        StageManager.getInstance().setPrimaryStage(primaryStage);
+
+        DataBean dataBean = new DataBean();
 
         CloudVC vc = new CloudVC(dataBean);
         vc.show();
