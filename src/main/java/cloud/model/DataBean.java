c763a74fb2cd1a6e2ac745a445ba0aff746eb924
Mirco <mircoboerm@gmx.de>
2019-11-03 17:47:06 +0100

    updated DataBean only for Data holding

--- src/main/java/cloud/model/DataBean.java
+++ src/main/java/cloud/model/DataBean.java
@@ -3,20 +3,13 @@ package cloud.model;
 import javafx.stage.Stage;
 
 public class DataBean {
-    private Stage primaryStage = null;
-
     private Session cloudSession;
 
-    public DataBean(Stage primaryStage) {
-        this.primaryStage = primaryStage;
+    public DataBean() {
         this.cloudSession = new Session();
     }
 
     public Session getSession() {
         return this.cloudSession;
-    }
-
-    public Stage getPrimaryStage() {
-        return primaryStage;
     }
 }
