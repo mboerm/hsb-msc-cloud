5a84607c80623156c151312cf669143502ac40cf
Mirco53 <mircoboerm@gmx.de>
2019-11-04 12:27:26 +0100

    global control button variables and created getter methods

--- src/main/java/cloud/view/designs/DesignControls.java
+++ src/main/java/cloud/view/designs/DesignControls.java
@@ -6,28 +6,31 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.VBox;
 
 public class DesignControls extends VBox {
+    private Button controlAdd;
+    private Button controlRemove;
+    private Button controlEdit;
 
     public DesignControls() {
-        Button controlAdd = new Button("Add");
-        Button controlRemove = new Button("Remove");
-        Button controlEdit = new Button("Edit");
+        controlAdd = new Button("Add");
+        controlRemove = new Button("Remove");
+        controlEdit = new Button("Edit");
 
         setSpacing(10);
         setMargin(controlAdd, new Insets(5, 5, 5, 5));
         setMargin(controlRemove, new Insets(5, 5, 5, 5));
         setMargin(controlEdit, new Insets(5, 5, 5, 5));
         getChildren().addAll(controlAdd, controlRemove, controlEdit);
+    }
 
-        controlAdd.setOnAction(actionEvent -> {
-            DialogAddComponent dialogAdd = new DialogAddComponent();
-        });
+    public Button getControlAdd() {
+        return this.controlAdd;
+    }
 
-        controlRemove.setOnAction(actionEvent -> {
+    public Button getControlRemove() {
+        return this.controlRemove;
+    }
 
-        });
-
-        controlEdit.setOnAction(actionEvent -> {
-
-        });
+    public Button getControlEdit() {
+        return this.controlEdit;
     }
 }
