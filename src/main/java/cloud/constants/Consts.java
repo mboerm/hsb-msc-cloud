e62af79fde57dee53d5f3b09e9c8da16fe86c43c
Mirco53 <mircoboerm@gmx.de>
2019-12-30 10:50:55 +0100

    minor changes in consts

--- src/main/java/cloud/constants/Consts.java
+++ src/main/java/cloud/constants/Consts.java
@@ -3,14 +3,15 @@ package cloud.constants;
 public final class Consts {
 
     public static final String APP_TITLE = "Cloud Architecture Optimizer";
-    public static final int WINDOW_DEFAULT_WIDTH = 1024;
-    public static final int WINDOW_DEFAULT_HEIGHT = 768;
-    public static final int WINDOW_MIN_WIDTH = 800;
-    public static final int WINDOW_MIN_HEIGHT = 600;
+    public static final int WINDOW_DEFAULT_WIDTH = 800;
+    public static final int WINDOW_DEFAULT_HEIGHT = 600;
+    public static final int WINDOW_MIN_WIDTH = 640;
+    public static final int WINDOW_MIN_HEIGHT = 480;
 
-    public enum COMPONENT_CATEGORIES {Default, Compute, Storage, Database, Analytics, Network, Integration, Management};
-    public enum INSTANCE_TYPES {Normal, Spot, Reserved};
-    public enum DATABASE_TYPES {SQL, MySQL, NoSQL, Document};
+    public enum COMPONENT_CATEGORIES {None, Compute, Storage, Database, Analytics, Network, Integration, Management};
+    public enum INSTANCE_TYPES {OnDemand, Reserved, Spot};
+    public enum DATABASE_TYPES {SQL, MariaDB, NoSQL, Document};
+    public enum COMPONENT_USAGE {On_Premise, Off_Premise};
 
     public static final String ABOUT_TEXT =
             "This tool is the result of the master thesis.";
