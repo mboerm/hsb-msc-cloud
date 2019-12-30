1b8af3d10ed084ea6bebccbf786d3c342aca4a91
Mirco53 <mircoboerm@gmx.de>
2019-12-30 10:51:24 +0100

    added more properties to component

--- src/main/java/cloud/model/Component.java
+++ src/main/java/cloud/model/Component.java
@@ -5,11 +5,15 @@ import static cloud.constants.Consts.*;
 public class Component {
 
     private String name;
+    private String service;
     private COMPONENT_CATEGORIES category;
+    private COMPONENT_USAGE usage;
 
     Component() {
         name = "Default";
-        category = COMPONENT_CATEGORIES.Default;
+        service = "Default";
+        category = COMPONENT_CATEGORIES.None;
+        usage = COMPONENT_USAGE.On_Premise;
     }
 
     public String getName() {
@@ -19,10 +23,24 @@ public class Component {
         this.name = name;
     }
 
+    public String getService() {
+        return this.service;
+    }
+    public void setService(String service) {
+        this.service = service;
+    }
+
     public COMPONENT_CATEGORIES getCategory() {
         return this.category;
     }
     public void setCategory(COMPONENT_CATEGORIES category) {
         this.category = category;
+    }
+
+    public COMPONENT_USAGE getUsage() {
+        return this.usage;
+    }
+    public void setUsage(COMPONENT_USAGE usage) {
+        this.usage = usage;
     }
 }
