a43211d12568156fc9afa2d5f4362fb5557554c3
Mirco53 <mircoboerm@gmx.de>
2020-01-04 21:02:08 +0100

    misc

--- src/main/java/cloud/model/StorageComponent.java
+++ src/main/java/cloud/model/StorageComponent.java
@@ -1,7 +1,5 @@
 package cloud.model;
 
-import cloud.model.Component;
-
 import static cloud.constants.Consts.*;
 
 public class StorageComponent extends Component {
@@ -9,6 +7,6 @@ public class StorageComponent extends Component {
     // TODO: Parameter für Storage hinzufügen
 
     public StorageComponent() {
-        setCategory(COMPONENT_CATEGORIES.Storage);
+        setCategory(COMPONENT_CATEGORY.Storage);
     }
 }
