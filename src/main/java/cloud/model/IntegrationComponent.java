5c8a0b0dcf8ceb15a4b3bf8bc715ec4b8a616d82
Mirco53 <mircoboerm@gmx.de>
2020-01-04 21:00:35 +0100

    added app integration component

---
+++ src/main/java/cloud/model/IntegrationComponent.java
@@ -0,0 +1,10 @@
+package cloud.model;
+
+import static cloud.constants.Consts.*;
+
+public class IntegrationComponent extends Component {
+
+    public IntegrationComponent() {
+        setCategory(COMPONENT_CATEGORY.Integration);
+    }
+}
