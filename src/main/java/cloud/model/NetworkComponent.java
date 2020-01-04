2f26d57836200318aaf506c9eabc1aed70506f38
Mirco53 <mircoboerm@gmx.de>
2020-01-04 21:00:50 +0100

    added network component

---
+++ src/main/java/cloud/model/NetworkComponent.java
@@ -0,0 +1,10 @@
+package cloud.model;
+
+import static cloud.constants.Consts.*;
+
+public class NetworkComponent extends Component {
+
+    public NetworkComponent() {
+        setCategory(COMPONENT_CATEGORY.Network);
+    }
+}
