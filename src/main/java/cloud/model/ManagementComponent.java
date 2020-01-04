58694965a936ae76b36cc5a5391f157cf8e6582c
Mirco53 <mircoboerm@gmx.de>
2020-01-04 21:00:43 +0100

    added management component

---
+++ src/main/java/cloud/model/ManagementComponent.java
@@ -0,0 +1,10 @@
+package cloud.model;
+
+import static cloud.constants.Consts.*;
+
+public class ManagementComponent extends Component {
+
+    public ManagementComponent() {
+        setCategory(COMPONENT_CATEGORY.Management);
+    }
+}
