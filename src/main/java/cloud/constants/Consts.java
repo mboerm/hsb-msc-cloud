33e432afa01830f38bde976588cc2be60729d1ee
Mirco53 <mircoboerm@gmx.de>
2020-01-04 20:58:07 +0100

    added more constants

--- src/main/java/cloud/constants/Consts.java
+++ src/main/java/cloud/constants/Consts.java
@@ -3,15 +3,21 @@ package cloud.constants;
 public final class Consts {
 
     public static final String APP_TITLE = "Cloud Architecture Optimizer";
-    public static final int WINDOW_DEFAULT_WIDTH = 800;
-    public static final int WINDOW_DEFAULT_HEIGHT = 600;
     public static final int WINDOW_MIN_WIDTH = 640;
     public static final int WINDOW_MIN_HEIGHT = 480;
 
-    public enum COMPONENT_CATEGORIES {None, Compute, Storage, Database, Analytics, Network, Integration, Management};
-    public enum INSTANCE_TYPES {OnDemand, Reserved, Spot};
-    public enum DATABASE_TYPES {SQL, MariaDB, NoSQL, Document};
-    public enum COMPONENT_USAGE {On_Premise, Off_Premise};
+    public enum COMPONENT_CATEGORY {None, Compute, Storage, Database, Analytics, Network, Integration, Management};
+    public enum COMPONENT_INSTANCE_SIZE {Micro, Small, Medium, Large, XLarge};
+    public enum COMPONENT_USAGE_TYPE {On_Demand, Reserved, Spot};
+    public enum COMPONENT_USAGE_PERIOD {Hourly, Monthly, One_Year, Three_Years};
+    public enum COMPONENT_USAGE_PREPAY {None, Partial, Full};
+    public enum COMPONENT_REGION {};
+    public enum COMPONENT_OPERATING_MODE {On_Premise, Off_Premise};
+
+    public enum COMPUTE_INSTANCE_TYPE {General, Compute, RAM, Storage, Burst, GPU}
+
+    public enum DATABASE_INSTANCE_TYPE {Standard, Storage};
+    public enum DATABASE_TYPE {SQL, MariaDB, NoSQL, Document};
 
     public static final String ABOUT_TEXT =
             "This tool is the result of the master thesis.";
