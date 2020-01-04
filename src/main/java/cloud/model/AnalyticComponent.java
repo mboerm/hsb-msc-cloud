d8732461f0fe63bcf29761262a29b2dc084d1e1d
Mirco53 <mircoboerm@gmx.de>
2020-01-04 21:00:24 +0100

    added analytic component

---
+++ src/main/java/cloud/model/AnalyticComponent.java
@@ -0,0 +1,10 @@
+package cloud.model;
+
+import static cloud.constants.Consts.*;
+
+public class AnalyticComponent extends Component {
+
+    public AnalyticComponent() {
+        setCategory(COMPONENT_CATEGORY.Analytics);
+    }
+}
