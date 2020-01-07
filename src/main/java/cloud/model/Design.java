a62cc45b90fe1012c017ce5de50636cd89ad828e
Mirco53 <mircoboerm@gmx.de>
2020-01-07 13:47:37 +0100

    added general attributes to design

--- src/main/java/cloud/model/Design.java
+++ src/main/java/cloud/model/Design.java
@@ -9,6 +9,14 @@ public class Design {
     private ArrayList<Component> components;
     private Component selectedComponent;
 
+    private String usagePeriod;
+    private String primaryRegion;
+    private String numOfInstances;
+    private String numOfRequests;
+    private String periodOfRequests;
+    private String numOfCapacity;
+    private String periodOfCapacity;
+
     public Design() {
         components = new ArrayList<>();
     }
@@ -31,4 +39,25 @@ public class Design {
     public void setSelectedComponent(Component comp) {
         this.selectedComponent = comp;
     }
+
+    public String getUsagePeriod() {return this.usagePeriod;}
+    public void setUsagePeriod(String period) {this.usagePeriod = period;}
+
+    public String getPrimaryRegion() {return this.primaryRegion;}
+    public void setPrimaryRegion(String region) {this.primaryRegion = region;}
+
+    public String getNumOfInstances() {return this.numOfInstances;}
+    public void setNumOfInstances(String instances) {this.numOfInstances = instances;}
+
+    public String getNumOfRequests() {return this.numOfRequests;}
+    public void setNumOfRequests(String requests) {this.numOfRequests = requests;}
+
+    public String getPeriodOfRequests() {return this.periodOfRequests;}
+    public void setPeriodOfRequests(String requestPeriod) {this.periodOfRequests = requestPeriod;}
+
+    public String getNumOfCapacity() {return this.numOfCapacity;}
+    public void setNumOfCapacity(String capacity) {this.numOfCapacity = capacity;}
+
+    public String getPeriodOfCapacity() {return this.periodOfCapacity;}
+    public void setPeriodOfCapacity(String capacityPeriod) {this.periodOfCapacity = capacityPeriod;}
 }
