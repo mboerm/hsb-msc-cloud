94679e673d0a28c0280edd3826b8e8ad471fee6a
Mirco53 <mircoboerm@gmx.de>
2020-01-07 13:48:19 +0100

    added general property ui controls for design

--- src/main/java/cloud/view/panes/PaneDesignProperties.java
+++ src/main/java/cloud/view/panes/PaneDesignProperties.java
@@ -1,18 +1,82 @@
 package cloud.view.panes;
 
+import cloud.config.Config;
 import javafx.geometry.Insets;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
 
 public class PaneDesignProperties extends GridPane {
 
-    public PaneDesignProperties() {
-        setPadding(new Insets(5, 5, 5, 5));
+    private TextField usagePeriodField;
+    private ComboBox regionComboBox;
+    private Spinner<Integer> numOfInstancesSpinner;
+    private Spinner<Integer> numOfRequestsSpinner;
+    private Spinner<Integer> numOfCapacitySpinner;
+    private ComboBox periodOfRequestsComboBox;
+    private ComboBox periodOfCapacityComboBox;
 
-        Label testLabel = new Label("Test: ");
-        TextField testField = new TextField();
-        add(testLabel, 0, 0);
-        add(testField, 1, 0);
+    public PaneDesignProperties() {
+        setPadding(new Insets(10, 10, 10, 10));
+        setVgap(10);
+        setHgap(10);
+
+        Label usagePeriodLbl = new Label("Primary usage period: ");
+        usagePeriodField = new TextField();
+        usagePeriodField.setPromptText("hh:mm - hh:mm");
+
+        Label regionLbl = new Label("Primary region: ");
+        regionComboBox = new ComboBox();
+        regionComboBox.getItems().addAll(Config.getInstance().getConfigValues("component-regions"));
+
+        Label numOfInstancesLbl = new Label("# of compute instances: ");
+        numOfInstancesSpinner = new Spinner<>(1, 100, 1);
+        numOfInstancesSpinner.setEditable(true);
+
+        Label numOfRequestsLbl = new Label("# of requests: ");
+        numOfRequestsSpinner = new Spinner<>(100000, 10000000, 100000);
+        numOfRequestsSpinner.setEditable(true);
+        periodOfRequestsComboBox = new ComboBox();
+        periodOfRequestsComboBox.getItems().addAll(Config.getInstance().getConfigValues("component-property-period"));
+
+        Label numOfCapacityLbl = new Label("# of storage capacity: ");
+        numOfCapacitySpinner = new Spinner<>(10, 10000, 10);
+        numOfCapacitySpinner.setEditable(true);
+        periodOfCapacityComboBox = new ComboBox();
+        periodOfCapacityComboBox.getItems().addAll(Config.getInstance().getConfigValues("component-property-period"));
+
+        add(usagePeriodLbl, 0, 0);
+        add(usagePeriodField, 1, 0);
+        add(regionLbl, 0, 1);
+        add(regionComboBox, 1, 1);
+        add(numOfInstancesLbl, 0, 2);
+        add(numOfInstancesSpinner, 1, 2);
+        add(numOfRequestsLbl, 0, 3);
+        add(numOfRequestsSpinner, 1, 3);
+        add(periodOfRequestsComboBox, 1, 4);
+        add(numOfCapacityLbl, 0, 5);
+        add(numOfCapacitySpinner, 1, 5);
+        add(periodOfCapacityComboBox, 1, 6);
+    }
+
+    public TextField getUsagePeriodField() {
+        return this.usagePeriodField;
+    }
+    public Spinner<Integer> getNumOfInstancesSpinner() {
+        return this.numOfInstancesSpinner;
+    }
+    public Spinner<Integer> getNumOfRequestsSpinner() {
+        return this.numOfRequestsSpinner;
+    }
+    public Spinner<Integer> getNumOfCapacitySpinner() {
+        return this.numOfCapacitySpinner;
+    }
+    public ComboBox getPrimaryRegionComboBox() {
+        return this.regionComboBox;
+    }
+    public ComboBox getPeriodOfRequestsComboBox() {
+        return this.periodOfRequestsComboBox;
+    }
+    public ComboBox getPeriodOfCapacityComboBox() {
+        return this.periodOfCapacityComboBox;
     }
 }
