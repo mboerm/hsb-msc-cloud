b236da504a1456ce4e8ffbf0d8e70dae84556e3d
Mirco53 <mircoboerm@gmx.de>
2020-01-07 07:11:47 +0100

    updated view controller

--- src/main/java/cloud/controller/CloudVC.java
+++ src/main/java/cloud/controller/CloudVC.java
@@ -2,15 +2,14 @@ package cloud.controller;
 
 import cloud.provider.ProviderFactory;
 import cloud.view.dialogs.DialogAddComponent;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.scene.control.Alert;
-import javafx.stage.FileChooser;
 import cloud.model.Design;
 import cloud.model.StageManager;
 import cloud.view.CloudView;
 
 import static cloud.constants.Consts.*;
-
-import java.io.File;
 
 public class CloudVC {
 
@@ -25,21 +24,11 @@ public class CloudVC {
         this.design = design;
         this.providerFactory = new ProviderFactory();
         this.view = new CloudView();
-        initViewHandler();
+        initMenuHandler();
+        initDesignAreaHandler();
     }
 
-    private void initViewHandler() {
-        view.getMenuFileNew().setOnAction(actionEvent -> newSession());
-
-        view.getMenuFileOpen().setOnAction(actionEvent -> {
-            FileChooser fileChooser = new FileChooser();
-            File selectedFile = fileChooser.showOpenDialog(StageManager.getInstance().getPrimaryStage());
-        });
-
-        view.getMenuFileSave().setOnAction(actionEvent -> {
-            FileChooser fileChooser = new FileChooser();
-            File savedFile = fileChooser.showSaveDialog(StageManager.getInstance().getPrimaryStage());
-        });
+    private void initMenuHandler() {
 
         view.getMenuFileExit().setOnAction(actionEvent -> System.exit(0));
 
@@ -53,7 +42,9 @@ public class CloudVC {
         view.getMenuServicesGoogle().setOnAction(actionEvent -> providerFactory.getProvider("Google"));
 
         view.getMenuHelpAbout().setOnAction(actionEvent -> showAboutDialog());
+    }
 
+    private void initDesignAreaHandler() {
         view.getPaneDesignArea().getComponentsTable().getSelectionModel().selectedItemProperty().addListener((obs, oldSelection, newSelection) -> {
             if (newSelection != null) {
                 getDesign().setSelectedComponent(newSelection);
@@ -70,9 +61,35 @@ public class CloudVC {
         view.getPaneDesignArea().getControlEdit().setOnAction(actionEvent -> {});
     }
 
-    private void newSession() {
-        this.design = new Design();
+    private void initDesignPropertyHandler() {
+        view.getPaneDesignProperties().getUsagePeriodField().textProperty().addListener((obs, oldValue, newValue) ->
+            getDesign().setUsagePeriod(newValue));
+
+        view.getPaneDesignProperties().getPrimaryRegionComboBox().getSelectionModel().selectedItemProperty().addListener((obs, oldValue, newValue) -> {
+            getDesign().setPrimaryRegion(newValue.toString());
+        });
+
+        view.getPaneDesignProperties().getNumOfInstancesSpinner().getEditor().textProperty().addListener((obs, oldValue, newValue) -> {
+            getDesign().setNumOfInstances(newValue);
+        });
+
+        view.getPaneDesignProperties().getNumOfRequestsSpinner().getEditor().textProperty().addListener((obs, oldValue, newValue) -> {
+            getDesign().setNumOfRequests(newValue);
+        });
+
+        view.getPaneDesignProperties().getPeriodOfRequestsComboBox().getSelectionModel().selectedItemProperty().addListener((obs, oldValue, newValue) -> {
+            getDesign().setPeriodOfRequests(newValue.toString());
+        });
+
+        view.getPaneDesignProperties().getNumOfCapacitySpinner().getEditor().textProperty().addListener((obs, oldValue, newValue) -> {
+            getDesign().setNumOfCapacity(newValue);
+        });
+
+        view.getPaneDesignProperties().getPeriodOfCapacityComboBox().getSelectionModel().selectedItemProperty().addListener((obs, oldValue, newValue) -> {
+            getDesign().setPeriodOfCapacity(newValue.toString());
+        });
     }
+
     private Design getDesign() {
         return this.design;
     }
