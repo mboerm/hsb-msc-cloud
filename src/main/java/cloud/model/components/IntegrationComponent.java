27f8821ee015a6d42dad43549eccb932f234458a
Mirco53 <mircoboerm@gmx.de>
2020-01-13 19:08:21 +0100

    added pane for integration component

--- src/main/java/cloud/model/components/IntegrationComponent.java
+++ src/main/java/cloud/model/components/IntegrationComponent.java
@@ -1,7 +1,64 @@
 package cloud.model.components;
+
 public class IntegrationComponent extends Component {
 
-    public IntegrationComponent() {
+    private String type;
+    private String data;
+    private String requests;
+
+    /**
+     * 0 = number of push messages
+     * 1 = number of http messages
+     * 2 = number of mail messages
+     * 3 = number of sms messages
+     */
+    private String[] messages = new String[4];
+
+    public IntegrationComponent(
+            String name,
+            String type,
+            String data,
+            String requests
+    ) {
+        setName(name);
         setCategory("Integration");
+        setType(type);
+        setData(data);
+        setRequests(requests);
+    }
+
+    public IntegrationComponent(
+            String name,
+            String type,
+            String data,
+            String requests,
+            String[] messages
+    ) {
+        setName(name);
+        setCategory("Integration");
+        setType(type);
+        setData(data);
+        setRequests(requests);
+        setMessages(messages);
+    }
+
+    public String getType() {
+        return type;
+    }
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getData() {return this.data;}
+    public void setData(String data) {this.data = data;}
+
+    public String getRequests() {return this.requests;}
+    public void setRequests(String requests) {this.requests = requests;}
+
+    public String[] getMessages() {
+        return messages;
+    }
+    public void setMessages(String[] messages) {
+        this.messages = messages;
     }
 }
