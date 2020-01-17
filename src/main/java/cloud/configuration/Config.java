c269db6573c3405f6bfdc1fe3f9e0d4b5826919c
Mirco53 <mircoboerm@gmx.de>
2020-01-17 21:14:04 +0100

    added more methods to get values

--- src/main/java/cloud/configuration/Config.java
+++ src/main/java/cloud/configuration/Config.java
@@ -1,5 +1,8 @@
 package cloud.configuration;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
 import java.io.*;
 import java.util.*;
 
@@ -13,7 +16,7 @@ import static cloud.configuration.Constants.*;
 public class Config {
 	private static volatile Config INSTANCE = null;
 
-	private Properties configs = null;
+	private Properties configs;
 
 	public static Config getInstance() {
     	if (INSTANCE == null) {
@@ -25,10 +28,10 @@ public class Config {
     private Config() {
 		configs = new Properties();
 		try {
-			String respath = CONFIG_FILE;
-			InputStream inStream = this.getClass().getClassLoader().getResourceAsStream(respath);
+			String path = CONFIG_FILE;
+			InputStream inStream = this.getClass().getClassLoader().getResourceAsStream(path);
 			if (inStream == null)
-				throw new Exception("resource not found: " + respath);
+				throw new Exception("resource not found: " + path);
 			configs.loadFromXML(inStream);
 		} catch (Exception ex) {
 			System.out.println("Error in get configuration value!" + ex);
@@ -36,20 +39,38 @@ public class Config {
 	}
 	
 	/**
-	 * Get value
-	 * @param configName
-	 * @return value as string
+	 * Get values of configuration parameter
+	 * @param configName name of configuration parameter
+	 * @return values as string-array
 	 */
-	public String[] getConfigValues(String configName) {
+	public String[] getConfigValuesAsArray(String configName) {
 		if (configName == null || configName.length() == 0) {
 			throw new IllegalArgumentException("Invalid configuration name!");
 		}
-		String result = null;
+		String result = "";
 		try {
 			result = configs.getProperty(configName);
 		} catch (Exception ex) {
 			System.out.println("Error in get configuration value!" + ex);
 		}
 		return result.split(CONFIG_SEPARATOR);
+	}
+
+	/**
+	 * Get single value of configuration parameter
+	 * @param configName name of configuration parameter
+	 * @return value as string
+	 */
+	public String getConfigValue(String configName) {
+		return getConfigValuesAsArray(configName)[0];
+	}
+
+	/**
+	 * Get values of configuration parameter
+	 * @param configName name of configuration parameter
+	 * @return values as observable list
+	 */
+	public ObservableList<String> getConfigValues(String configName) {
+		return FXCollections.observableArrayList(getConfigValuesAsArray(configName));
 	}
 }
