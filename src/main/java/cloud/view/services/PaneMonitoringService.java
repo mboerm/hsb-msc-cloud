cf00fdb80e6d3cd35a8ce736bad85b4d20b5758d
Mirco53 <mircoboerm@gmx.de>
2020-01-17 21:15:56 +0100

    updated pane for monitoring service

--- src/main/java/cloud/view/services/PaneMonitoringService.java
+++ src/main/java/cloud/view/services/PaneMonitoringService.java
@@ -6,10 +6,10 @@ import javafx.scene.control.Spinner;
 
 public class PaneMonitoringService extends PaneServiceProperties {
 
-    private Spinner metricsSpinner;
-    private Spinner requestsSpinner;
-    private Spinner dataSpinner;
-    private Spinner eventsSpinner;
+    private Spinner<String> metricsSpinner;
+    private Spinner<String> requestsSpinner;
+    private Spinner<String> dataSpinner;
+    private Spinner<String> eventsSpinner;
     private CheckBox isLoggingBox;
 
     public PaneMonitoringService() {
@@ -31,20 +31,20 @@ public class PaneMonitoringService extends PaneServiceProperties {
 
         isLoggingBox = new CheckBox();
 
-        add(metricsLbl, 0, 1);
-        add(metricsSpinner, 1, 1);
-        add(new Label("per month"), 2, 1);
-        add(requestsLbl, 0, 2);
-        add(requestsSpinner, 1, 2);
+        add(metricsLbl, 0, 2);
+        add(metricsSpinner, 1, 2);
         add(new Label("per month"), 2, 2);
-        add(dataLbl, 0, 3);
-        add(dataSpinner, 1, 3);
+        add(requestsLbl, 0, 3);
+        add(requestsSpinner, 1, 3);
         add(new Label("per month"), 2, 3);
-        add(eventsLbl, 0, 4);
-        add(eventsSpinner, 1, 4);
+        add(dataLbl, 0, 4);
+        add(dataSpinner, 1, 4);
         add(new Label("per month"), 2, 4);
-        add(isLoggingLbl, 0, 5);
-        add(isLoggingBox, 1, 5);
+        add(eventsLbl, 0, 5);
+        add(eventsSpinner, 1, 5);
+        add(new Label("per month"), 2, 5);
+        add(isLoggingLbl, 0, 6);
+        add(isLoggingBox, 1, 6);
     }
 
     public String getMetrics() {return this.metricsSpinner.getEditor().getText();}
