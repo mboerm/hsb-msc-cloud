0ad3935109da46817cf6c932cf2f94b0c1a95b2e
Mirco53 <mircoboerm@gmx.de>
2020-01-19 10:20:55 +0100

    updated network service creator

--- src/main/java/cloud/model/services/NetworkServiceCreator.java
+++ src/main/java/cloud/model/services/NetworkServiceCreator.java
@@ -1,5 +1,7 @@
 package cloud.model.services;
 
+import cloud.configuration.Config;
+
 public class NetworkServiceCreator implements ServiceAbstractCreator {
 
     private String name;
@@ -20,17 +22,16 @@ public class NetworkServiceCreator implements ServiceAbstractCreator {
 
     @Override
     public Service createService() {
-        switch (this.type) {
-            case "VPC":
-            case "VPN":
-            case "API":
-                return new NetworkService(name, type, requests, data, "", "");
-            case "CDN":
-                return new NetworkService(name, type, requests, data, dataOut, "");
-            case "DNS":
-                return new NetworkService(name, type, requests, "", "", zones);
-            default:
-                return null;
+        String[] types = Config.getInstance().getConfigValuesAsArray("network-type");
+
+        if (type.equals(types[0]) || type.equals(types[1]) || type.equals(types[2])) {
+            return new NetworkService(name, type, requests, data, "", "");
+        } else if (type.equals(types[3])) {
+            return new NetworkService(name, type, requests, data, dataOut, "");
+        } else if (type.equals(types[4])) {
+            return new NetworkService(name, type, requests, "", "", zones);
+        } else {
+            return null;
         }
     }
 }
