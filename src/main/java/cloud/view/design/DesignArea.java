df090693645c61050d8a39394d23e89bb2da622b
Mirco53 <mircoboerm@gmx.de>
2020-01-20 21:41:50 +0100

    updated table view for display name of service

--- src/main/java/cloud/view/design/DesignArea.java
+++ src/main/java/cloud/view/design/DesignArea.java
@@ -17,12 +17,12 @@ public class DesignArea extends VBox {
 
     private void initArea() {
         TableColumn<Service, String> serviceNameCol = new TableColumn<>("Name");
-        TableColumn<Service, String> serviceTypeCol = new TableColumn<>("Service");
+        TableColumn<Service, String> serviceDisplayCol = new TableColumn<>("Service");
         TableColumn<Service, String> serviceProviderNameCol = new TableColumn<>("Provider-Service");
         TableColumn<Service, String> serviceCategoryCol = new TableColumn<>("Category");
 
         serviceNameCol.setCellValueFactory(new PropertyValueFactory<>("name"));
-        serviceTypeCol.setCellValueFactory(new PropertyValueFactory<>("service"));
+        serviceDisplayCol.setCellValueFactory(new PropertyValueFactory<>("displayName"));
         serviceProviderNameCol.setCellValueFactory(new PropertyValueFactory<>("providerService"));
         serviceCategoryCol.setCellValueFactory(new PropertyValueFactory<>("category"));
 
@@ -30,10 +30,10 @@ public class DesignArea extends VBox {
         servicesTable.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
 
         serviceNameCol.setMaxWidth( 1f * Integer.MAX_VALUE * 30 );
-        serviceTypeCol.setMaxWidth( 1f * Integer.MAX_VALUE * 30 );
+        serviceDisplayCol.setMaxWidth( 1f * Integer.MAX_VALUE * 30 );
         serviceProviderNameCol.setMaxWidth(1f * Integer.MAX_VALUE * 30);
         serviceCategoryCol.setMaxWidth( 1f * Integer.MAX_VALUE * 30 );
-        servicesTable.getColumns().addAll(serviceNameCol, serviceTypeCol, serviceProviderNameCol, serviceCategoryCol);
+        servicesTable.getColumns().addAll(serviceNameCol, serviceDisplayCol, serviceProviderNameCol, serviceCategoryCol);
     }
 
     public TableView<Service> getServicesTable() {
