78daef160aeede9030dbf962fab8151778419208
Mirco53 <mircoboerm@gmx.de>
2020-02-28 11:07:15 +0100

    added comments to constants class

--- src/main/java/cloud/configuration/Constants.java
+++ src/main/java/cloud/configuration/Constants.java
@@ -1,8 +1,10 @@
 package cloud.configuration;
 
 import java.text.*;
-import java.util.Date;
 
+/**
+ * Constants class
+ */
 public final class Constants {
     private Constants(){
         /*
@@ -12,23 +14,22 @@ public final class Constants {
         throw new AssertionError();
     }
 
-    public static final String APP_TITLE = "Cloud Architecture Optimizer";
-    public static final String DIALOG_SERVICE_TITLE = "Service Dialog";
-    public static final String DIALOG_SERVICE_HEADER = "Select service category and define properties";
-    public static final String DIALOG_COSTS_TITLE = "Cost Calculation Report";
+    /* Window and dialog size */
     public static final int WINDOW_MIN_WIDTH = 900;
     public static final int WINDOW_MIN_HEIGHT = 500;
     public static final int DIALOG_MIN_WIDTH = 400;
     public static final int DIALOG_MIN_HEIGHT = 200;
 
+    /* Path to config file and separator */
     public static final String CONFIG_FILE = "xml/config.xml";
     public static final String CONFIG_SEPARATOR = ";";
 
+    /* Decimal and Date format */
     public static final DecimalFormat DOUBLE_FORMAT_2 = new DecimalFormat("#0.00");
-    public static final DateFormat DATE_FORMAT_FILE = new SimpleDateFormat("dd-MM-yy_HH-mm-ss");
+    public static final DateFormat DATE_FORMAT_FILE = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss");
     public static final DateFormat DATE_FORMAT_TITLE = new SimpleDateFormat("d. MMMM yyyy");
-//            DateFormat.getDateInstance(DateFormat.MEDIUM);
 
+    /* Cost calculation factors */
     public static final int HOUR_SECONDS = 3600;
     public static final int MONTH_SECONDS = 2628000;
     public static final int MONTH_HOURS = 730;
